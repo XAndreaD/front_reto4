996f1c8c59c2d7f3c5364b7dc634c1d16f2658cd
Andrea <xiomara.diaz.mt@correo.usa.edu.co>
2021-11-05 12:48:47 -0500

    firts commit

---
+++ js/misFunciones.js
@@ -0,0 +1,378 @@
+function autoInicioCategoria(){
+    console.log("se esta ejecutando")
+    $.ajax({
+        url:"http://168.138.71.93:8081/api/Category/all",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarRespuesta(respuesta);
+            let $select = $("#select-category");
+            $.each(respuesta, function (id, name) {
+                $select.append('<option value='+name.id+'>'+name.name+'</option>');
+                console.log("select "+name.id);
+            }); 
+        }
+    
+    })
+
+}
+
+function pintarRespuesta(respuesta){
+
+    let myTable="<table>";
+    for(i=0;i<respuesta.length;i++){
+        myTable+="<tr>";
+        myTable+="<td>"+respuesta[i].name+"</td>";
+        myTable+="<td>"+respuesta[i].description+"</td>";
+        myTable+="<td> <button onclick=' actualizarInformacionCategorias("+respuesta[i].id+")'>Actualizar</button>";
+        myTable+="<td> <button onclick='borrarCategoria("+respuesta[i].id+")'>Borrar</button>";
+        myTable+="</tr>";
+    }
+    myTable+="</table>";
+    $("#resultado1").html(myTable);
+}
+
+function traerInformacionCategoria(){
+    $.ajax({
+        url:"http://168.138.71.93:8081/api/Cabin/all",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarRespuesta(respuesta);
+        }
+    });
+}
+
+function guardarInformacionCategorias(){
+    let var2 = {
+        name:$("#Cname").val(),
+        description:$("#Cdescription").val()
+        };
+      
+        $.ajax({
+        type:'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'JSON',
+        data: JSON.stringify(var2),
+        
+        url:"http://168.138.71.93:8081/api/Category/save",
+       
+        
+        success:function(response) {
+                console.log(response);
+            console.log("Se guardo correctamente");
+            alert("Se guardo correctamente");
+            window.location.reload()
+    
+        },
+        
+        error: function(jqXHR, textStatus, errorThrown) {
+              window.location.reload()
+            alert("No se guardo correctamente");
+    
+    
+        }
+        });
+
+}
+
+function actualizarInformacionCategorias(idElemento){
+    let myData={
+        id:idElemento,
+        name:$("#Cname").val(),
+        description:$("#Cdescription").val()
+
+    };
+    console.log(myData);
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"http://168.138.71.93:8081/api/Category/update",
+        type:"PUT",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            $("#id").val("");
+            $("#Cname").val("");
+            $("#Cdescription").val("");
+            autoInicioCategoria();
+            alert("se ha Actualizado correctamente la categoria")
+        }
+    });
+
+}
+
+function borrarCategoria(idElemento){
+    let myData={
+        id:idElemento
+    };
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"http://168.138.71.93:8081/api/Category/"+idElemento,
+        type:"DELETE",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            autoInicioCategoria();
+            alert("Se ha Eliminado.")
+        }
+    });
+
+}
+////////////Tabla Cabin////////////////////
+
+function autoInicioCabin(){
+    console.log("se esta ejecutando")
+    $.ajax({
+        url:"http://168.138.71.93:8081/api/Cabin/all",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarRespuesta1(respuesta);
+            let $select = $("#select-cabin");
+            $.each(respuesta, function (id, name) {
+                $select.append('<option value='+name.id+'>'+name.name+'</option>');
+                console.log("select "+name.id);
+            }); 
+        }
+    
+    })
+
+}
+function pintarRespuesta1(respuesta){
+
+    let myTable="<table>";
+    for(i=0;i<respuesta.length;i++){
+        myTable+="<tr>";
+        myTable+="<td>"+respuesta[i].name+"</td>";
+        myTable+="<td>"+respuesta[i].brand+"</td>";
+        myTable+="<td>"+respuesta[i].rooms+"</td>";
+        myTable+="<td>"+respuesta[i].description+"</td>";
+        myTable+="<td>"+respuesta[i].category.name+"</td>";
+        myTable+="<td> <button onclick=' actualizarInformacionOrtesis("+respuesta[i].id+")'>Actualizar</button>";
+        myTable+="<td> <button onclick='borrarOrtesis("+respuesta[i].id+")'>Borrar</button>";
+        myTable+="</tr>";
+    }
+    myTable+="</table>";
+    $("#resultado2").html(myTable);
+}
+
+function guardarInformacionCabin(){
+    let var2 = {
+        name:$("#Cname").val(),
+        brand:$("#Cbrand").val(),
+        year:$("#Crooms").val(),
+        description:$("#Cdescription").val(),
+        category: {id:+$("#select-category").val()},
+        };
+       
+        console.log(var2);
+        $.ajax({
+        type:'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'JSON',
+        data: JSON.stringify(var2),
+        
+        url:"http://168.138.71.93:8081/api/Cabin/save",
+       
+        
+        success:function(response) {
+                console.log(response);
+            console.log("Se guardo correctamente");
+            alert("Se guardo correctamente");
+            window.location.reload()
+    
+        },
+        
+        error: function(jqXHR, textStatus, errorThrown) {
+              window.location.reload()
+            alert("No se guardo correctamente");
+    
+    
+        }
+        });
+
+}
+
+function actualizarInformacionCabin(idElemento){
+    let myData={
+        id:idElemento,
+        name:$("#Cname").val(),
+        brand:$("#Cbrand").val(),
+        year:$("#Crooms").val(),
+        description:$("#Cdescription").val(),
+        category: {id:+$("#select-category").val()},
+
+
+    };
+    console.log(myData);
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"http://168.138.71.93:8081/api/Cabin/update",
+        type:"PUT",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            $("#id").val("");
+            $("#Cname").val("");
+            $("#Cbrand").val("");
+            $("#Crooms").val("");
+            $("#Cdescription").val("");
+            $("#select-category").val("");
+            autoInicioOrtesis();
+            alert("se ha Actualizado correctamente Cabin")
+        }
+    });
+
+}
+
+function borrarCabin(idElemento){
+    let myData={
+        id:idElemento
+    };
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"http://168.138.71.93:8081/api/Cabin/"+idElemento,
+        type:"DELETE",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            autoInicioOrtesis();
+            alert("Se ha Eliminado.")
+        }
+    });
+
+}
+/////////Tabla Cliente////////////////////////////
+
+function autoInicioCliente(){
+    console.log("se esta ejecutando")
+    $.ajax({
+        url:"http://168.138.71.93:8081/api/Client/all",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarRespuesta2(respuesta);
+        }
+    
+    })
+
+}
+function pintarRespuesta2(respuesta){
+
+    let myTable="<table>";
+    for(i=0;i<respuesta.length;i++){
+        myTable+="<tr>";
+        
+        myTable+="<td>"+respuesta[i].email+"</td>";
+        myTable+="<td>"+respuesta[i].password+"</td>";
+        myTable+="<td>"+respuesta[i].name+"</td>";
+        myTable+="<td>"+respuesta[i].age+"</td>";
+        myTable+="<td> <button onclick=' actualizarInformacionCliente("+respuesta[i].idClient+")'>Actualizar</button>";
+        myTable+="<td> <button onclick='borrarCliente("+respuesta[i].idClient+")'>Borrar</button>";
+        myTable+="</tr>";
+    }
+    myTable+="</table>";
+    $("#resultado3").html(myTable);
+}
+
+function guardarInformacionCliente(){
+    let var2 = {
+        
+        email:$("#Clemail").val(),
+        password:$("#Clpassword").val(),
+        name:$("#Clname").val(),
+        age:$("#Clage").val(),
+     
+        };
+       
+        console.log(var2);
+        $.ajax({
+        type:'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'JSON',
+        data: JSON.stringify(var2),
+        
+        url:"http://168.138.71.93:8081/api/Client/save",
+       
+        
+        success:function(response) {
+                console.log(response);
+            console.log("Se guardo correctamente");
+            alert("Se guardo correctamente");
+            window.location.reload()
+    
+        },
+        
+        error: function(jqXHR, textStatus, errorThrown) {
+              window.location.reload()
+            alert("No se guardo correctamente");
+    
+    
+        }
+        });
+
+}
+
+function actualizarInformacionCliente(idElemento){
+    let myData={
+        idClient:idElemento,
+        email:$("#Clemail").val(),
+        password:$("#Clpassword").val(),
+        name:$("#Clname").val(),
+        age:$("#Clage").val(),
+
+
+    };
+    console.log(myData);
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"http://168.138.71.93:8081/api/Client/update",
+        type:"PUT",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            $("#idClient").val("");
+            $("#Clemail").val("");
+            $("#Clpassword").val("");
+            $("#Clname").val("");
+            $("#Clage").val("");
+            autoInicioCliente();
+            alert("se ha Actualizado correctamente Cliente")
+        }
+    });
+
+}
+
+function borrarCliente(idElemento){
+    let myData={
+        idClient:idElemento
+    };
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"http://168.138.71.93:8081/api/Client/"+idElemento,
+        type:"DELETE",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            autoInicioCliente();
+            alert("Se ha Eliminado.")
+        }
+    });
+
+}
